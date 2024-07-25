8d36465e9c679f00736f8d8039e41f3052d861af
Dagim Alemu <dagimalemux@gmail.com>
2024-07-25 17:16:19 +0300

    image configuration in next config js file locally

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'localhost',
+      "gorgeous-nature-b643a8bf9d.media.strapiapp.com"
+
+    ],
+  },
 }
 
 module.exports = nextConfig
