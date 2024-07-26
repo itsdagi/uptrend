b0cf0a33c82718fbd82fbc88f7d56fdb599d1344
Dagim Alemu <dagimalemux@gmail.com>
2024-07-26 13:56:52 +0300

    fix next config js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
+  
   images: {
     domains: [
      
       "localhost",
       "images.pexels.com",
-      
       "gorgeous-nature-b643a8bf9d.media.strapiapp.com",
 
     ],
   },
-}
+};
 module.exports = nextConfig
