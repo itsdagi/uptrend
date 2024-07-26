d9a5c5122fdd90bd452d31376cee83da7a8b14bb
Dagim Alemu <dagimalemux@gmail.com>
2024-07-26 16:02:16 +0300

    change images to image

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   
-  images: {
+  image: {
     domains: [
-     
+      "gorgeous-nature-b643a8bf9d.media.strapiapp.com",
     ],
   },
 };
